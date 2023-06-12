d1e440ad028806a1e5d0e82bee4826c5fb126bec
jace <nic.chiron@gmail.com>
2023-06-12 18:48:01 +0200

    add validation model report

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,8 @@
 class Report < ApplicationRecord
   belongs_to :user
   belongs_to :status
+
+  validates :address, presence: true
+  validates :content, presence: true, length: {minimum: 30}
+  validates :title, presence: true, length: {minimum: 15, maximum:60}
 end
