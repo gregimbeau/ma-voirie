98b301139add2d93e7d82e5753592cc64ce20538
jace <nic.chiron@gmail.com>
2023-06-12 18:33:20 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Report.delete_all
+User.delete_all
+
+
+array_status = [
+  "Validé",
+  "Accepté",
+  "En cours",
+  "Résolu"
+]
+
+if Status.count == 0
+  4.times do
+    status = Status.create!(
+    title: array_status.first 
+    )
+    array_status.rotate!
+  end
+end
+
+2.times do
+  user = User.create!(
+    nickname: Faker::Name.first_name,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, 
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    phone_number: Faker::PhoneNumber.phone_number,
+    age: Faker::Number.within(range: 20..80),
+    is_admin: false
+  )
+end
+
+5.times do
+  report = Report.create!(
+    title: Faker::Book.title,
+    content: Faker::Movie.quote,
+    is_validate: nil,
+    address: Faker::Address.street_address,
+    user_id: User.all.sample.id,
+    status_id: Status.all.sample.id
+  )
+end
