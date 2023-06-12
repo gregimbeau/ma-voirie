0ecc26b2f09fe5657006b11a7060140ba6965f9a
jace <nic.chiron@gmail.com>
2023-06-12 16:27:47 +0200

    add status and modification models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_130917) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_141145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,10 +19,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_130917) do
     t.text "content"
     t.boolean "validate"
     t.string "address"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
+    t.bigint "status_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["status_id"], name: "index_reports_on_status_id"
     t.index ["user_id"], name: "index_reports_on_user_id"
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +51,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_130917) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reports", "users"
 end
