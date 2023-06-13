8e615084b885a50d2ba194471fa7e9a239ea52bc
jace <nic.chiron@gmail.com>
2023-06-13 10:57:50 +0200

    permit user account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:nickname, :first_name, :last_name, :age, :email, :phone_number, :password, :password_confirmation)}
-    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:nickname, :first_name, :last_name, :age, :email, :phone_number, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:nickname, :first_name, :last_name, :age, :email, :phone_number, :current_password, :password, :password_confirmation)}
   end
 
 end
