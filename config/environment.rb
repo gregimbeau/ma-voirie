2d67c6c9cab7598ca94dca0f493b8894343e6465
Guillaume Regimbeau <gregimbeau@mail.com>
2023-06-13 18:36:19 +0200

    mail views updated

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,13 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'gregimbeau@gmail.com',
-  :password => ENV['BREVO_PWD'],
-  :domain => 'monsite.fr',
-  :address => 'smtp-relay.sendinblue.com',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+:user_name => 'apikey',
+:password => ENV['SENDGRID_PWD'],
+:domain => 'monsite.fr',
+:address => 'smtp.sendgrid.net',
+:port => 587,
+:authentication => :plain,
+:enable_starttls_auto => true
 }
