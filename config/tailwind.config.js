b149ab6bc0a5c42868d0f9bf5b7e2d80dc7bf233
vincent <kratos_angel_59@hotmail.fr>
2023-06-15 19:20:43 +0200

    Admin dashboard responsive

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-    },
+    },   
   },
   plugins: [
     require('@tailwindcss/forms'),
