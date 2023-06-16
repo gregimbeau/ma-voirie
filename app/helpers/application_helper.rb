6601dad1e440a9b7069987d3952392eddea9d9ca
vincent <kratos_angel_59@hotmail.fr>
2023-06-16 17:04:48 +0200

    edit registration form checkbox + edit controller app_helper valid status on index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,11 @@ module ApplicationHelper
   end
 
   def check_if_admin_or_creator
-    unless current_user&.is_admin || current_user&.id == Report.find(params['id']).user_id
-      flash[:alert] = "Cet évènement n'est pas encore validé !"
-      redirect_to root_path
+    if Report.find(params['id']).status_id == 1
+      unless current_user&.is_admin || current_user&.id == Report.find(params['id']).user_id
+        flash[:alert] = "Cet évènement n'est pas encore validé !"
+        redirect_to root_path
+      end
     end
   end
 end
