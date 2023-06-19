9a4b71180287928b08172ea758211d376cc020b2
Guillaume Regimbeau <gregimbeau@mail.com>
2023-06-19 14:28:53 +0200

    reduction interligne pour meilleur responsivité

--- db/schema.rb
+++ db/schema.rb
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_103858) do
     t.boolean "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
