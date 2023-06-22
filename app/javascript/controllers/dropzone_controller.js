9b079023c700e8a0a522326078932cf818577d17
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-06-22 15:53:40 +0200

    dropzone test

--- app/javascript/controllers/dropzone_controller.js
+++ app/javascript/controllers/dropzone_controller.js
@@ -1,13 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
-import Dropzone from 'dropzone';
+import Dropzone from 'controllers/dropzone_controller';
 import { DirectUpload } from "@rails/activestorage";
-import {
-  getMetaValue,
-  findElement,
-  removeElement,
-  insertAfter,
-} from "../helpers/dropzone";
 
+export function getMetaValue(name) {
+  const element = findElement(document.head, `meta[name="${name}"]`)
+  if (element) {
+    return element.getAttribute("content")
+  }
+}
+
+export function findElement(root, selector) {
+  if (typeof root == "string") {
+    selector = root
+    root = document
+  }
+  return root.querySelector(selector)
+}
+
+export function removeElement(el) {
+  if (el && el.parentNode) {
+    el.parentNode.removeChild(el);
+  }
+}
+
+export function insertAfter(el, referenceNode) {
+    return referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+}
 
 // Connects to data-controller="dropzone"
 export default class extends Controller {
