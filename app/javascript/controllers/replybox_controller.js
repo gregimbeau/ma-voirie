7531d116da8ec34ddd012e955aff5a492eee2827
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-06-22 12:28:35 +0200

    turbo comments / likes

--- app/javascript/controllers/replybox_controller.js
+++ app/javascript/controllers/replybox_controller.js
@@ -10,10 +10,12 @@ function buttonsAnswerClicks() {
   var answer_buttons = document.querySelectorAll("#reply_show");
   var reply_div = document.querySelectorAll("#reply_div");
 
-  for (let i = 0 ; i < answer_buttons.length ; i++) {
-    answer_buttons[i].addEventListener("click", (e) => {
-      reply_div[i].classList.remove("hidden");
-    });
+  if (answer_buttons!=null && reply_div!=null) {
+    for (let i = 0 ; i < answer_buttons.length ; i++) {
+      answer_buttons[i].addEventListener("click", (e) => {
+        reply_div[i].classList.remove("hidden");
+      });
+    }
   }
 }
 
@@ -24,46 +26,44 @@ function buttonsSendReplyClicks() {
   var textarea_main_comment = document.querySelector("#textarea_main_comment");
   var button_main_comment = document.querySelector("#button_main_comment");
 
-  for (let i = 0 ; i < reply_buttons.length ; i++) {
-    if (reply_textareas[i].value.length<1)
-    {
-      reply_buttons[i].setAttribute("disabled","disabled");
-      reply_buttons[i].classList.remove("cstm-bg-action");
-      reply_buttons[i].classList.remove("cstm-light");
-      reply_buttons[i].classList.add("cstm-bg-dark");
-      reply_buttons[i].async = true;
-    } else {
-      reply_buttons[i].removeAttribute("disabled");
-      reply_buttons[i].classList.remove("cstm-bg-dark");
-      reply_buttons[i].classList.add("cstm-bg-action");
-      reply_buttons[i].classList.add("cstm-light");
-      reply_buttons[i].async = true;
+  if (reply_buttons!=null) {
+    for (let i = 0 ; i < reply_buttons.length ; i++) {
+      if (reply_textareas[i].value.length<1)
+      {
+        reply_buttons[i].setAttribute("disabled","disabled");
+        reply_buttons[i].classList.remove("cstm-bg-action");
+        reply_buttons[i].classList.remove("cstm-light");
+        reply_buttons[i].classList.add("cstm-bg-dark");
+        reply_buttons[i].async = true;
+      } else {
+        reply_buttons[i].removeAttribute("disabled");
+        reply_buttons[i].classList.remove("cstm-bg-dark");
+        reply_buttons[i].classList.add("cstm-bg-action");
+        reply_buttons[i].classList.add("cstm-light");
+        reply_buttons[i].async = true;
+      }
     }
   }
 
-  if (textarea_main_comment.value.length < 1)
-  {
-    button_main_comment.setAttribute("disabled","disabled");
-    button_main_comment.classList.remove("cstm-bg-action");
-    button_main_comment.classList.remove("cstm-light");
-    button_main_comment.classList.add("cstm-bg-dark");
-    button_main_comment.async = true;
-  } else {
-    button_main_comment.removeAttribute("disabled");
-    button_main_comment.classList.remove("cstm-bg-dark");
-    button_main_comment.classList.add("cstm-bg-action");
-    button_main_comment.classList.add("cstm-light");
-    button_main_comment.async = true;
+  if (textarea_main_comment!=null) {
+    if (textarea_main_comment.value.length < 1)
+    {
+      button_main_comment.setAttribute("disabled","disabled");
+      button_main_comment.classList.remove("cstm-bg-action");
+      button_main_comment.classList.remove("cstm-light");
+      button_main_comment.classList.add("cstm-bg-dark");
+      button_main_comment.async = true;
+    } else {
+      button_main_comment.removeAttribute("disabled");
+      button_main_comment.classList.remove("cstm-bg-dark");
+      button_main_comment.classList.add("cstm-bg-action");
+      button_main_comment.classList.add("cstm-light");
+      button_main_comment.async = true;
+    }
   }
 }
 
-document.addEventListener("turbo:load", ()=>{
-  buttonsAnswerClicks();
-  buttonsSendReplyClicks();
-});
-
-buttonsAnswerClicks();
-
 setInterval(() => {
+  buttonsAnswerClicks();
   buttonsSendReplyClicks();
 }, 100);
